ce85b12a4c529a5f01590f35cd0f6c657145a2dd
caseyboller <caseyeboller@gmail.com>
2021-04-02 17:29:06 +1030

    Got box working in Babylon and added the dorna glb and extra babylon packages, haven't got dorna working yet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import vb from 'vue-babylonjs'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(vb)
 
 new Vue({
   router,
--- src/views/Simulation.vue
+++ src/views/Simulation.vue
@@ -1,22 +1,54 @@
+
 <template>
-<div>
-  {{this.userSelection["body"]}} with {{this.userSelection.engine}} engine and {{this.userSelection.powerup}} powerup
-</div>
+  <div id="renderCanvas">
+    <!-- {{this.userSelection["body"]}} with {{this.userSelection.engine}} engine and {{this.userSelection.powerup}} powerup -->
+    <Scene v-model="myScene">
+      <Entity v-model="myEntity">
+        <Box v-model="myBox"></Box>
+      </Entity>
+      <!-- <Asset src="../assets/dornaRigged.glb" v-model="myAssetRoot"></Asset> -->
+    </Scene>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: 'Simulation',
   props: {
     test: {one:"a",two:"a"}//should be passing values as props but don't know how yet
   },
   data() {
-    return {userSelection: {body: "", engine: "", powerup: ""}, mode: 0};
+    return {
+      userSelection: {body: "", engine: "", powerup: ""}, 
+      mode: 0,
+      myScene: null,
+      myEntity: null,
+      myBox: null,
+      // myAssetRoot: {path: '../assets/dornaRigged.glb'},
+      };
+  },
+  watch: {
+    myScene() {
+      // myScene is now available from the component
+      // do something with it here or call a method to use it from here
+    },
+
+    myEntity() {
+      // myEntity is now available from the component
+      // do something with it here or call a method to use it from here
+    },
+
+    myBox() {
+      // myBox is now available from the component
+      // do something with it here or call a method to use it from here
+    }
   },
   mounted() {
     this.userSelection["body"] = this.$route.query.body;
     this.userSelection["engine"] = this.$route.query.engine;
     this.userSelection["powerup"] = this.$route.query.powerup;
+    
   }
 }
 </script>
