503ab34ca7a20b63a653d79f9ccf840b435412d2
Casey Boller <caseyeboller@gmail.com>
2021-04-06 02:00:03 +0930

    Got dorna arm working in babylon with raw vue, fixed quaternion rotation issue

--- src/views/Simulation.vue
+++ src/views/Simulation.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-  {{this.userSelection["body"]}} with {{this.userSelection.engine}} engine and {{this.userSelection.powerup}} powerup
+  <div>{{this.userSelection["body"]}} with {{this.userSelection.engine}} engine and {{this.userSelection.powerup}} powerup</div>
+  <canvas id="gameCanvas"></canvas>
 </div>
 </template>
 
@@ -8,16 +9,43 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, Mesh, MeshBuilder, HemisphericLight } from "@babylonjs/core";
+import { Skeleton, SceneLoader, Engine, Scene, ArcRotateCamera, Vector3, Mesh, MeshBuilder, HemisphericLight } from "@babylonjs/core";
 import Vue from 'vue'
 
-var createScene = function (engine, canvas) {
+var createScene = async function (engine, canvas) {
   var scene = new Scene(engine);
   var camera =  new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
   camera.attachControl(canvas, true);
   var light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-  var sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-  // hide/show the Inspector
+  // var box = MeshBuilder.CreateBox("box", { diameter: 1 }, scene);
+  // const ground = MeshBuilder.CreateGround("ground", 1, 1, 1, scene, false);
+  SceneLoader.ImportMeshAsync("", "/assets/", "dornaRigged.glb").then((result) => {
+    console.log(result)
+    for (var mesh in result.meshes) {
+      var thisMesh = result.meshes[mesh]
+      console.log(thisMesh.name)
+      var rot = thisMesh.rotationQuaternion.toEulerAngles()
+      // console.log(Math.round(rot.x * 2) / 2)
+      // console.log(Math.round(rot.y * 2) / 2)
+      // console.log(Math.round(rot.z * 2) / 2)
+      // Quaternions must be reset on imported models otherwise they will not be able to be rotated
+      thisMesh.rotationQuaternion = null;
+      // But we still want them in the original positions
+      thisMesh.rotation = Vector3((rot.x * 2) / 2, (rot.y * 2) / 2, (rot.z * 2) / 2)
+
+    }
+
+    console.log(scene.getMeshByName("Arm_1"));
+    
+
+    // result.meshes[0].scaling.scaleInPlace(0.8)
+    // result.meshes[1].position.x = 20;
+    // const dornaArm1 = scene.getMeshByName("Arm_1");
+    // console.log(dornaArm1);
+
+
+  });
+
 
   return scene;
 }
@@ -25,14 +53,14 @@ var createScene = function (engine, canvas) {
 class BabylonApp {
     constructor() {
         // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        var canvas = document.getElementById("gameCanvas");
         // initialize babylon scene and engine
         var engine = new Engine(canvas, true);
-        var scene = createScene(engine, canvas) 
+        var scene;
+        var scenePromise = createScene(engine, canvas) 
+        scenePromise.then(returnedScene => {
+          scene = returnedScene;
+        })
 
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
